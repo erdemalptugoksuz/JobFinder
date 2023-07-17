95be3a64b00d3a6e2f77b040fe3190ff9a55bd83
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2023-07-17 19:11:54 +0300

    Update Router.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import {Stacks} from './components';
+
+const Stack = createNativeStackNavigator();
 
 const Router = () => {
   return (
-    <View>
-      <Text>Router</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Stacks">
+        <Stack.Screen
+          name="Stacks"
+          component={Stacks}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
