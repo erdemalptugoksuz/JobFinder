4b94ab1a66288853846f004f177911e5c0bbb0fa
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2023-07-17 19:12:54 +0300

    Added Job Details screen

---
+++ src/screens/JobDetails/JobDetails.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const JobDetails = () => {
+  return (
+    <SafeAreaView>
+      <Text>JobDetails</Text>
+    </SafeAreaView>
+  );
+};
+
+export default JobDetails;
---
+++ src/screens/index.ts
@@ -0,0 +1,5 @@
+import Home from './Home/Home';
+import JobDetails from './JobDetails/JobDetails';
+import Search from './Search/Search';
+
+export {Home, JobDetails, Search};
