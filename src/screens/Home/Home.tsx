a6f97f55c190a2f53b3bfce1b98464e2eee45793
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2023-07-18 13:16:54 +0300

    Update Home.tsx

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, View, ScrollView} from 'react-native';
 
-const Home = () => {
+import styles from './Home.style';
+import {Welcome, PopularJobs, NearbyJobs} from '../../components';
+
+const Home = ({navigation}: any) => {
+  const {container, innerContainer} = styles;
   return (
-    <SafeAreaView>
-      <Text>Home</Text>
+    <SafeAreaView style={container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={innerContainer}>
+          <Welcome navigation={navigation} />
+          <PopularJobs navigation={navigation} />
+          <NearbyJobs navigation={navigation} />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
