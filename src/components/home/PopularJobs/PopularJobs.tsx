3428f6cc7b8984ad12cdc89c53ce0b8c03166fdc
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2023-07-18 12:41:56 +0300

    Added PopularJobs component

---
+++ src/components/home/PopularJobs/PopularJobs.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import styles from './PopularJobs.style';
+
+const PopularJobs = () => {
+  return (
+    <View>
+      <Text>PopularJobs</Text>
+    </View>
+  );
+};
+
+export default PopularJobs;
