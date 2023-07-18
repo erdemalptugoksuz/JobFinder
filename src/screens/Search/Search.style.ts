8236e36d25acb825fb2dd725a2f352c05dcd761a
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2023-07-18 15:49:46 +0300

    Finished Search screen

--- src/screens/Search/Search.style.ts
+++ src/screens/Search/Search.style.ts
@@ -1,3 +1,53 @@
 import {StyleSheet} from 'react-native';
 
-export default StyleSheet.create({});
+import {COLORS, SIZES} from '../../constants';
+
+export default StyleSheet.create({
+  container: {
+    width: '100%',
+  },
+  searchTitle: {
+    fontSize: SIZES.xLarge,
+    color: COLORS.primary,
+  },
+  noOfSearchedJobs: {
+    marginTop: 2,
+    fontSize: SIZES.small,
+    color: COLORS.primary,
+  },
+  loaderContainer: {
+    marginTop: SIZES.medium,
+  },
+  footerContainer: {
+    marginTop: SIZES.small,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    gap: 10,
+  },
+  paginationButton: {
+    width: 30,
+    height: 30,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: COLORS.tertiary,
+  },
+  paginationImage: {
+    width: '60%',
+    height: '60%',
+    tintColor: COLORS.white,
+  },
+  paginationTextBox: {
+    width: 30,
+    height: 30,
+    borderRadius: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: COLORS.white,
+  },
+  paginationText: {
+    fontSize: SIZES.medium,
+    color: COLORS.primary,
+  },
+});
--- src/screens/Search/Search.tsx
+++ src/screens/Search/Search.tsx
@@ -1,10 +1,119 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text,
+  ActivityIndicator,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+import axios from 'axios';
+import {RAPID_API_KEY} from '@env';
+import Icon from 'react-native-vector-icons/Feather';
 
-const Search = () => {
+import {NearbyJobCard} from '../../components';
+import {COLORS, SIZES} from '../../constants';
+import styles from './Search.style';
+
+const rapidApiKey = RAPID_API_KEY;
+
+const Search = ({navigation, route}: any) => {
+  const [searchResult, setSearchResult] = useState([]);
+  const [searchLoader, setSearchLoader] = useState(false);
+  const [searchError, setSearchError] = useState(null);
+  const [page, setPage] = useState(1);
+
+  const handleSearch = async () => {
+    setSearchLoader(true);
+    setSearchResult([]);
+
+    try {
+      const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/search`,
+        headers: {
+          'X-RapidAPI-Key': rapidApiKey,
+          'X-RapidAPI-Host': 'jsearch.p.rapidapi.com',
+        },
+        params: {
+          query: route.params.item,
+          page: page.toString(),
+        },
+      };
+
+      const response = await axios.request(options);
+      setSearchResult(response.data.data);
+    } catch (error: any) {
+      setSearchError(error);
+      console.log(error);
+    } finally {
+      setSearchLoader(false);
+    }
+  };
+
+  const handlePagination = (direction: any) => {
+    if (direction === 'left' && page > 1) {
+      setPage(page - 1);
+      handleSearch();
+    } else if (direction === 'right') {
+      setPage(page + 1);
+      handleSearch();
+    }
+  };
+
+  useEffect(() => {
+    handleSearch();
+  }, []);
+
+  const handleNearbyJobDetailNavigate = (id: any) => {
+    navigation.navigate('JobDetailsScreen', {id});
+  };
+
   return (
-    <SafeAreaView>
-      <Text>Search</Text>
+    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
+      <FlatList
+        data={searchResult}
+        renderItem={({item}) => (
+          <NearbyJobCard
+            job={item}
+            handleNavigate={() => handleNearbyJobDetailNavigate(item?.job_id)}
+          />
+        )}
+        keyExtractor={item => item.job_id}
+        contentContainerStyle={{padding: SIZES.medium, rowGap: SIZES.medium}}
+        ListHeaderComponent={() => (
+          <>
+            <View style={styles.container}>
+              <Text style={styles.searchTitle}>{route.params.item}</Text>
+              <Text style={styles.noOfSearchedJobs}>Job Opportunities</Text>
+            </View>
+            <View style={styles.loaderContainer}>
+              {searchLoader ? (
+                <ActivityIndicator size="large" color={COLORS.primary} />
+              ) : (
+                searchError && <Text>Oops something went wrong</Text>
+              )}
+            </View>
+          </>
+        )}
+        ListFooterComponent={() => (
+          <View style={styles.footerContainer}>
+            <TouchableOpacity
+              style={styles.paginationButton}
+              onPress={() => handlePagination('left')}>
+              <Icon name="chevron-left" size={30} color={COLORS.primary} />
+            </TouchableOpacity>
+            <View style={styles.paginationTextBox}>
+              <Text style={styles.paginationText}>{page}</Text>
+            </View>
+            <TouchableOpacity
+              style={styles.paginationButton}
+              onPress={() => handlePagination('right')}>
+              <Icon name="chevron-right" size={30} color={COLORS.primary} />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
     </SafeAreaView>
   );
 };
