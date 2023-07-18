779f66441d4cb5dbb05f71f4c8bbf7d3de8d6906
Erdem Alptuğ Öksüz <81254185+erdemalptugoksuz@users.noreply.github.com>
2023-07-18 12:42:03 +0300

    Added Welcome component

---
+++ src/components/home/Welcome/Welcome.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import styles from './Welcome.style';
+
+const Welcome = () => {
+  return (
+    <View>
+      <Text>Welcome</Text>
+    </View>
+  );
+};
+
+export default Welcome;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,9 @@
-import Stacks from './Stacks/Stacks';
+// General components
+import Stacks from './general/Stacks/Stacks';
 
-export {Stacks};
+// Home components
+import Welcome from './home/Welcome/Welcome';
+import PopularJobs from './home/PopularJobs/PopularJobs';
+import NearbyJobs from './home/NearbyJobs/NearbyJobs';
+
+export {Stacks, Welcome, PopularJobs, NearbyJobs};
